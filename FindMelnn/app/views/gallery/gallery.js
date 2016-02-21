4b6218f6accd675305224dbec815877f32a9d867
spv <stellavalcheva@yahoo.com>
2016-02-21 17:00:11 +0200

    anime

--- FindMelnn/app/views/gallery/gallery.js
+++ FindMelnn/app/views/gallery/gallery.js
@@ -1,12 +1,20 @@
 var galleryViewModelModule = require("./gallery-view-model");
+var viewModule = require("ui/core/view");
 
 var viewModel;
+
+
 function navigatingTo(args) {
     var page = args.object;
     viewModel = new galleryViewModelModule.GalleryViewModel();
     viewModel.photos = args.context;
+    viewModel.grid = viewModule.getViewById(page, "myGrid");
     page.bindingContext = viewModel;
-    console.log(viewModel.photos);
+}
+
+function onImageLongPressed(args) {
+	viewModel.onImageLongPressed(args);
 }
 
 exports.navigatingTo = navigatingTo;
+exports.onImageLongPressed = onImageLongPressed;
