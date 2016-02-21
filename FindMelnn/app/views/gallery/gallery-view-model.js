ad194927f1063b818d7e9fa2d42b235e5ae3b3ab
spv <stellavalcheva@yahoo.com>
2016-02-21 21:28:25 +0200

    image view amnimation

--- FindMelnn/app/views/gallery/gallery-view-model.js
+++ FindMelnn/app/views/gallery/gallery-view-model.js
@@ -1,6 +1,6 @@
 var viewModelBaseModule = require("../../common/view-model-base");
 var navigationModule = require("../../common/navigation");
-var imageModule = require("ui/image");
+var viewsModule = require("../../common/views");
 var timer = require("timer");
 
 var GalleryViewModel = (function (_super) {
@@ -9,35 +9,13 @@ var GalleryViewModel = (function (_super) {
         _super.call(this);
     }
 
-    GalleryViewModel.prototype.onImageLongPressed = function(args) {
-    	this.pressedImageSrc = this.photos[args.index].large;
-    	console.log(this.photos[args.index].large);
-    	
-    	var _weakThis = this;
-    	var timerId = timer.setInterval(function (){
-			_weakThis.grid._cols[0].value -= 0.1;
-			_weakThis.grid._cols[1].value += 0.1;
-			console.log(_weakThis.grid._cols[1].value);
-    	}, 100);
-
-    	timer.setTimeout(function (){
-			timer.clearInterval(timerId);
-    	}, 1000);
+    GalleryViewModel.prototype.onImageTap = function(args) {
+    	navigationModule.navigateTo({
+		moduleName: viewsModule.views.image,
+		backstackVisible: true,
+		context: this.photos[args.index]
+	});
     };
-
- 	Object.defineProperty(GalleryViewModel.prototype, "pressedImageSrc", {
-        get: function () {
-            return this._pressedImageSrc;
-        },
-        set: function (value) {
-            if (this._pressedImageSrc !== value) {
-                this._pressedImageSrc = value;
-                this.notifyPropertyChange("pressedImageSrc", value);
-            }
-        },
-        enumerable: true,
-        configurable: true
-    });
 
     return GalleryViewModel;
 
