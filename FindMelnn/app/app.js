8514c43c73b42a8a7119dd43d14fa2ba62ec49fa
slim <anarhisthristov@gmail.com>
2016-02-21 17:01:07 +0200

    db done

--- FindMelnn/app/app.js
+++ FindMelnn/app/app.js
@@ -1,21 +1,14 @@
 var application = require("application");
 var usersServices = require("./services/users");
 var viewsModule = require("./common/views");
- var dbModule = require("./common/dbContext");
- var hotelsServices = require("./services/hotels");
-// var dbModule = require("./common/dbContext");
-var applicationSettingsModule = require("application-settings");
-
- dbModule.dbInit();
-// hotelsServices.hotels.addHotel();
-// hotelsServices.hotels.getAll();
-// hotelsServices.hotels.removeHotel("1");
-// hotelsServices.hotels.getAll();
-// console.log("here");
+var dbModule = require("./common/dbContext");
+var hotelsServices = require("./services/hotels");
 
 application.cssFile = "./app.css";
 
 application.onLaunch = function(context) {
+    dbModule.dbInit();
+    
     if (usersServices.users.isSignedIn) {
         application.mainModule = viewsModule.views.search;
     } else {
