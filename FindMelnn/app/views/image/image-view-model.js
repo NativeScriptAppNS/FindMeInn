cd701ec7583357058dba733e04906e2608e89ef3
spv <stellavalcheva@yahoo.com>
2016-02-22 13:48:08 +0200

    pinch and scale animation

--- FindMelnn/app/views/image/image-view-model.js
+++ FindMelnn/app/views/image/image-view-model.js
@@ -1,12 +1,20 @@
 var viewModelBaseModule = require("../../common/view-model-base");
 var navigationModule = require("../../common/navigation");
+var viewsModule = require("../../common/views");
+var navigationModule = require("../../common/navigation");
+var frameModule = require("ui/frame");
 var timer = require("timer");
 
+var topmost = frameModule.topmost();
 var ImageViewModel = (function (_super) {
     __extends(ImageViewModel, _super);
     function ImageViewModel() {
         _super.call(this);       
     }
+
+    ImageViewModel.prototype.goBackToGallery = function(args) {
+    	navigationModule.goBack();
+    };
     
     return ImageViewModel;
 
